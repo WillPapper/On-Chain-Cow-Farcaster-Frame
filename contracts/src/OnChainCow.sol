7f4960c86929180014af7dc9c2859f0c75232cd9
Will Papper <contact@papper.me>
2024-01-27 13:28:30 -0800

    feat: Add OnChainCow NFT

---
+++ contracts/src/OnChainCow.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.8.20;
+
+import {ERC721} from "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+
+contract OnChainCow is ERC721 {
+    uint256 currentTokenId = 0;
+
+    constructor() ERC721("OnChainCow", "COW") {}
+
+    function mint(address to) public {
+        ++currentTokenId;
+        _mint(to, currentTokenId);
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        // Use Syndicate's API for dynamic metadata
+        // See the documentation at https://docs.syndicate.io/guides/dynamic-nft-metadata
+        return string(abi.encodePacked(
+            "https://metadata.syndicate.io/",
+            block.chainid, "/",
+            address(this), "/",
+            tokenId
+        ));
+}
