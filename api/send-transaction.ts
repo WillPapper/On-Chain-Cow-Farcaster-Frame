e8a5d691ff2fa9008f58b98aafcf3e1137234f4d
Will Papper <contact@papper.me>
2024-01-27 12:48:21 -0800

    feat: Migrated to On-Chain Cow settings

--- api/send-transaction.ts
+++ api/send-transaction.ts
@@ -10,6 +10,9 @@ const syndicate = new SyndicateClient({
   token: () => {
     const apiKey = process.env.SYNDICATE_API_KEY;
     if (typeof apiKey === "undefined") {
+      // If you receive this error, you need to define the SYNDICATE_API_KEY in
+      // your Vercel environment variables. You can find the API key in your
+      // Syndicate project settings under the "API Keys" tab.
       throw new Error(
         "SYNDICATE_API_KEY is not defined in environment variables."
       );
@@ -20,33 +23,19 @@ const syndicate = new SyndicateClient({
 
 export default async function (req: VercelRequest, res: VercelResponse) {
   try {
-    // Burn the Circularity NFT from the prior month, minting the next month in
-    // the process
-    const burnTx = await syndicate.transact.sendTransaction({
-      projectId: "2bee69fa-0e96-42cb-a864-8e7258a044a3",
-      contractAddress: "0x8680db891ff8c34f2ffcefac43d55059d010a821",
-      chainId: 1,
-      functionSignature: "burn()",
-    });
-
-    // Mint the Circularity Burn Ceremony NFT for the current month
-    // A higher amount could be passed in manually if this falls behind via
-    // query params, but this won't be needed now
+    // Mint the On-Chain Cow NFT. We're not passing in any arguments, since the
+    // amount will always be 1
     const mintTx = await syndicate.transact.sendTransaction({
-      projectId: "2bee69fa-0e96-42cb-a864-8e7258a044a3",
-      contractAddress: "0x83cc08E3783E9fF097e87a63BEc7503465C84714",
-      chainId: 1,
-      functionSignature: "mint(uint256 amount)",
-      args: {
-        amount: 1,
-      },
+      projectId: "abcab73a-55d2-4441-a93e-edf95d183b34",
+      contractAddress: "",
+      chainId: 84532,
+      functionSignature: "mint()",
     });
 
     res.status(200).json({
-      message: "Transactions submitted!",
+      message: "On-Chain Cow minted!",
       transactions: {
-        circularityBurn: burnTx.transactionId,
-        circularityBurnCeremonyMint: mintTx.transactionId,
+        onChainCow: mintTx.transactionId,
       },
     });
   } catch (error) {
