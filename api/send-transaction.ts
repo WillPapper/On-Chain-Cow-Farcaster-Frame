50bef4ee6737569b4c501b5cd7c2b4d36e666af8
Will Papper <contact@papper.me>
2024-01-27 15:11:43 -0800

    feat: Add Farcaster Frame to Vercel function

--- api/send-transaction.ts
+++ api/send-transaction.ts
@@ -22,22 +22,43 @@ const syndicate = new SyndicateClient({
 });
 
 export default async function (req: VercelRequest, res: VercelResponse) {
-  try {
-    // Mint the On-Chain Cow NFT. We're not passing in any arguments, since the
-    // amount will always be 1
-    const mintTx = await syndicate.transact.sendTransaction({
-      projectId: "abcab73a-55d2-4441-a93e-edf95d183b34",
-      contractAddress: "0xBeFD018F3864F5BBdE665D6dc553e012076A5d44",
-      chainId: 84532,
-      functionSignature: "mint(address to)",
-      args: {
-        // TODO: Change to the user's connected Farcaster address. This is going
-        // to WillPapper.eth for now
-        to: "0x3Cbd57dA2F08b3268da07E5C9038C11861828637",
-      },
-    });
-
+  // If the request is not a POST, we know that we're not dealing with a
+  // Farcaster Frame button click. Therefore, we should send the Farcaster Frame
+  // content
+  if (req.method !== "POST") {
     res.status(200).send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta property="fc:frame" content="vNext" />
+        <meta
+          property="fc:frame:image"
+          content="https://on-chain-cow-farcaster-frame.vercel.app/img/on-chain-cow-neutral-cow.png"
+        />
+        <meta property="fc:frame:button:1" content="Mint your On-Chain Cow!" />
+      </head>
+    </html>
+    `);
+    // Farcaster Frames will send a POST request to this endpoint when the user
+    // clicks the button. Therefore, if we are dealing with a POST request, we
+    // can assume that we're responding to a Farcaster Frame button click
+  } else {
+    try {
+      // Mint the On-Chain Cow NFT. We're not passing in any arguments, since the
+      // amount will always be 1
+      const mintTx = await syndicate.transact.sendTransaction({
+        projectId: "abcab73a-55d2-4441-a93e-edf95d183b34",
+        contractAddress: "0xBeFD018F3864F5BBdE665D6dc553e012076A5d44",
+        chainId: 84532,
+        functionSignature: "mint(address to)",
+        args: {
+          // TODO: Change to the user's connected Farcaster address. This is going
+          // to WillPapper.eth for now
+          to: "0x3Cbd57dA2F08b3268da07E5C9038C11861828637",
+        },
+      });
+
+      res.status(200).send(`
       <!DOCTYPE html>
       <html>
         <head>
@@ -50,7 +71,8 @@ export default async function (req: VercelRequest, res: VercelResponse) {
         </head>
       </html>
     `);
-  } catch (error) {
-    res.status(500).send(`Error: ${error.message}`);
+    } catch (error) {
+      res.status(500).send(`Error: ${error.message}`);
+    }
   }
 }
